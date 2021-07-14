dbdf1434cd93c4bc1b9300a62c006296a25947eb
ilya2nx <mishanin_95@mail.ru>
2021-07-14 12:57:48 +0300

    done lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { CreateComponent } from "./create/create.component";
 
 
 const routes: Routes = [  
-  {path: '', component: CreateComponent },
+  {path: '', component: CreateComponent, pathMatch: 'full' },
   {path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)},
-  {path: 'create/:id', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)},
+  {path: 'create/:id', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)}
 ]
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
