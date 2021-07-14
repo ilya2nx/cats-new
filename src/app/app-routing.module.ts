2639488eb1fd1d538b12950fea70a30fde041ae5
ilya2nx <mishanin_95@mail.ru>
2021-07-14 13:55:09 +0300

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
-import { CreateComponent } from "./create/create.component";
+import { ListComponent } from "./list/list.component";
+import { TileComponent } from "./tile/tile.component";
 
 
 const routes: Routes = [  
-  {path: '', component: CreateComponent, pathMatch: 'full' },
+  {path: '', component: ListComponent, children: [
+    {path:'', component: TileComponent}
+  ] },
   {path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)},
-  {path: 'create/:id', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)}
+  {path: 'create/:id', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)},
 ]
 
 @NgModule({
